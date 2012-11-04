0ed4590ad862c022b3a6761cc77808b5587e6fac
Rocco Tripaldi <tripaldir@gmail.com>
2012-11-03 22:49:43 -0400

    Fixed some bugs

--- wp-content/plugins/textclips/textclips.php
+++ wp-content/plugins/textclips/textclips.php
@@ -107,7 +107,7 @@ function save_textclip_data() {
   $textclip_show_edit_link = (isset($_POST['show_edit_link'])) ? true : false;
   // either update or add the show_edit_link bolean
   if(!update_post_meta($post_id, 'textclip_show_edit_link', $textclip_show_edit_link))
-    add_post_meta($post_id, 'textclip_show_edit_link', $textclip_show_edit_link)
+    add_post_meta($post_id, 'textclip_show_edit_link', $textclip_show_edit_link);
 
 }
  
