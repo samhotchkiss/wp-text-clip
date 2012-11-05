6b2d07f9ecd9c382fd07316c07a825a908c5ff0d
Rocco Tripaldi <tripaldir@gmail.com>
2012-11-04 20:56:32 -0500

    Created a Page Template that displays a text clip

---
+++ wp-content/themes/twentyeleven/page-with-contact-textclip.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Page with Contact Info textclip
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+				<div class='textclip'>
+					<?php echo get_text_clip(32); ?>
+				</div>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
