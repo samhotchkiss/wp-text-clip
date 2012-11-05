7afa00ea4598b1e819d85c1292c77f15f3251bdb
Rocco Tripaldi <tripaldir@gmail.com>
2012-11-04 21:00:26 -0500

    Removed the comments template from the contact page

--- wp-content/themes/twentyeleven/page-with-contact-textclip.php
+++ wp-content/themes/twentyeleven/page-with-contact-textclip.php
@@ -12,8 +12,6 @@ get_header(); ?>
 
 					<?php get_template_part( 'content', 'page' ); ?>
 
-					<?php comments_template( '', true ); ?>
-
 				<?php endwhile; // end of the loop. ?>
 
 				<div class='textclip'>
